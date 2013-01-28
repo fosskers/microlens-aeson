7edc6926634c21e219e7ff8b5f219dc03715254d
status failed <sf@statusfailed.com>
2013-01-28 09:37:53 +0000

    Removed TH code, and added Primitive and Value files

--- src/Data/Aeson/Traversal/Primitive.hs
+++ src/Data/Aeson/Traversal/Primitive.hs
@@ -1,4 +1,4 @@
-module Data.Aeson.Traversal.Types where
+module Data.Aeson.Traversal.Primitive where
 
 import Data.Text(Text)
 import Data.Attoparsec.Number(Number)
@@ -6,13 +6,14 @@ import Data.Attoparsec.Number(Number)
 -- Aeson main library
 import Data.Aeson
 
--- | Primitives from 'Value'
+-- | Primitives of 'Value'
 data Primitive
   = StringP !Text
   | NumberP !Number
   | BoolP   !Bool
   | NullP
 
+-- | Try to convert a Value to a Primitive
 toPrim :: Value -> Either Value Primitive
 toPrim (String s) = Right $ StringP s
 toPrim (Number n) = Right $ NumberP n
@@ -20,6 +21,7 @@ toPrim (Bool b)   = Right $ BoolP b
 toPrim Null       = Right $ NullP
 toPrim v          = Left v
 
+-- | Safely convert a Primitive back into a Value
 fromPrim :: Primitive -> Value
 fromPrim (StringP s) = String s
 fromPrim (NumberP n) = Number n
